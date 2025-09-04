8b662dafceecfafdb0744f5002bea6214b9852fc
S Aziz Khan <sakatazizkhan1@gmail.com>
2025-09-04 13:39:47 +0530

    feat: integrate Composer autoloading and update .gitignore on child theme

---
+++ wp-content/themes/mobilo-wp-child-theme/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mobilocard/mobilo-child-theme',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2bd047cfe3eb84929f2b15039c285840dd3fc82f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mobilocard/mobilo-child-theme' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2bd047cfe3eb84929f2b15039c285840dd3fc82f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
